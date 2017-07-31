5b7825400a8710134547ced0c1290ff2bc8acc55
Daniel-Hoerauf <dah4pe@virginia.edu>
2017-07-31 14:10:04 -0400

    Fixed for updated library

--- clojure.clj
+++ clojure.clj
@@ -1,23 +1,16 @@
 (fn [state time-left]
   (def arena-size (get-arena-size state))
-  (def arena-half (/ arena-size 2))
-  (def shot-range (:shot-distance game-parameters))
   
   (defn pick-move
       "Select the move to give the highest amount of points"
       [arena self]
-      (if (can-shoot-enemy? (get-direction arena) (add-locs arena) arena-size shot-range)
+      (if (can-shoot (get-direction arena) arena arena-size :wall true)
           (build-resp :shoot)
           (if (empty? (filter-arena (focus-sight arena) "food"))
-              (if (can-shoot-barrier? (get-direction arena) (add-locs arena) arena-size shot-range)
+              (if (can-shoot (get-direction arena) arena arena-size)
                   (build-resp :shoot)
-                  (move-to arena arena-half (get-direction arena) (select-target arena arena-half self) self))
-              (move-to arena arena-half (get-direction arena) (select-target-nowall arena arena-half self) self))))
+                  (move-to arena arena-size (get-direction arena) (select-target arena arena-size :wombat self) self))
+              (move-to arena arena-size (get-direction arena) (select-target arena arena-size :wombat self :wall false) self))))
 
     {:command (pick-move (:arena state) {:x 3 :y 3})
-     :state {:move (pick-move (:arena state) {:x 3 :y 3})
-             :saved (merge-global-state (get-global-state state :saved-state :saved) state arena-size)
-             :direction (get-direction (:arena state))
-             :shootable (can-shoot-enemy? (get-direction (:arena state)) (add-locs (:arena state)) arena-size shot-range)
-             :distance (distance-to-tile (get-direction (:arena state)) {:x 4 :y 3} arena-half)}})
-             
+     :state {}})
